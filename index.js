2467f412bd740bea378efbf06cfa8d4f84abda4b
abellasjuan <juanvictoraa@gmail.com>
2022-01-28 21:10:38 -0300

    feat: post messages

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ const participantSchema = joi.object({
     name: joi.string().required(),
 });
 
+const messagesSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.valid('message','private_message').required()
+});
+
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 mongoClient.connect(() => {
@@ -74,6 +80,44 @@ app.get('/participants', async (req, res) => {
         res.sendStatus(500);
     }
 });
+
+app.post('/messages', async (req, res) => {
+
+    try{
+        const { to, text, type } = req.body;
+        const { user } = req.headers;
+
+        const participantRegistered = await db.collection('participants').findOne({ name: user });
+    
+        if(!participantRegistered){
+            return res.status(422).send('O usuário "'+ user + '" não está cadastrado!');
+        }    
+        
+        const validation = messagesSchema.validate(req.body, { abortEarly: false });
+
+        if (validation.error) {
+            return res.status(422).send(validation.error.details.map(error => error.message));
+        }
+
+        const time = dayjs().locale('pt-br').format('HH:mm:ss');
+
+        await db.collection('messages').insertOne(
+            { 
+                "from": user,
+                "to": to,
+                "text": text,
+                "type": type,
+                "time": time 
+            }
+        );
+
+        res.sendStatus(201);
+
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+})
 
 app.listen(5000, ()=> (
     console.log('SERVER ON'))
