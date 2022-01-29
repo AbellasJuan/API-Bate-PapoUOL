4d611bb186091d51a328608e904602e2da9da97b
abellasjuan <juanvictoraa@gmail.com>
2022-01-28 21:05:51 -0300

    feat: post participants

--- index.js
+++ index.js
@@ -1,19 +1,68 @@
-import express from 'express';
+import express, { json } from 'express';
+import cors from 'cors';
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+import joi from 'joi';
+import dayjs from 'dayjs';
 
+dotenv.config();
 const app = express();
-app.use(express.json());
+app.use(json());
+app.use(cors());
 
-const results = [];
-
-app.post('/teste', (req, res) => {
-    const result = req.body;
-    results.push(result)
-    res.sendStatus(201);
+const participantSchema = joi.object({
+    name: joi.string().required(),
 });
 
-app.get('/teste', (req, res) => {
-    res.status(200).send(results);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+    db = mongoClient.db("batePapoUOL");
 });
+
+app.post('/participants', async (req, res) => {
+    try {
+        const { name } = req.body;
+
+        const alreadyRegistered = await db.collection('participants').findOne({ name: name });
+
+        if (alreadyRegistered) {
+            return res.status(409).send("Nome de usuário já existente")
+        }
+
+        const validation = participantSchema.validate(req.body, { abortEarly: false });
+
+        if (validation.error) {
+            return res.status(422).send(validation.error.details.map(error => error.message));
+        }
+
+        const time = dayjs().locale('pt-br').format('HH:mm:ss');
+
+        await db.collection('participants').insertOne(
+            { 
+                "name": name,
+                "lastStatus": Date.now() 
+            }
+        );
+
+        await db.collection('messages').insertOne(
+            { 
+                "from": name,
+                "to": "Todos",
+                "text": "entra na sala...",
+                "type": "message", 
+                "time": time 
+            }
+        );
+
+        res.sendStatus(201);
+
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+});
+
 
 app.listen(5000, ()=> (
     console.log('SERVER ON'))
